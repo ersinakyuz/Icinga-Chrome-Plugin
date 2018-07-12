539e65ea7849d84c892684cabee55526bead8677
Ersin Akyuz <eakyuz@gmx.net>
2018-07-12 15:32:53 +0300

    Web Notifications added.
    Changes in 0.4
    Notifications added. (Currently interval time 60 seconds, customization will be added in future)

--- background.js
+++ background.js
@@ -1,10 +1,43 @@
 // background.js
+var pollInterval = 1000 * 60; // 1 minute, in milliseconds
+var timerId;
 
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-  });
-});
+function startRequest() {
+    updateBadge();
+    timerId = window.setTimeout(startRequest, pollInterval);
+}
+function stopRequest() {
+    window.clearTimeout(timerId);
+}
+
+function showError() {
+    var xhr = new XMLHttpRequest;
+    var NagiosURL = "https://icinga/icingaweb2/monitoring/list/services?_host_department=IcingaServisYonetimi&_host_environment=PROD&service_state!=0&service_state!=99&service!=Check%20Pending%20Reboot%20&service!=Check%20Service&service!=Linux%20Service%20Check%20By%20Snmp&service!=Check%20File%20Size&service!=Arcsight%20Audit%20Policy&service!=Linux%20Centrify%20Check%20By%20Snmp&service!=Check%20Uptime&service!=Linux%20Uptime%20Check%20By%20Snmp&service!=Check%20Zpool&service!=Linux%20Cluster%20Check%20By%20Snmp&service!=Check%20Event%20ID%20System&_host_dc=DC1&service_acknowledged=0&service_in_downtime=0&host_in_downtime=0&limit=500&sort=service_severity&format=json"
+    xhr.open("GET", NagiosURL, false);
+    xhr.send(null);
+
+    if (xhr.status == 200) {
+        //var data = xhr.responseXML;
+        var data = JSON.parse(xhr.responseText);
+        console.log(data);
+        for (var lines in data) {
+            var sunucu = data[lines].host_name;
+            var hata = data[lines].service_output;
+            var durum = data[lines].service_state;
+            var servisadi = data[lines].service_display_name
+            if (durum == 2) {
+                sunuculine = durum + " - " + sunucu + " - " + servisadi;
+                document.write(sunuculine);
+                document.write("\n");
+                var notification = new Notification(sunucu, {icon: 'icon.png', body: hata, tag: "CRITICAL!"})
+                notification.onshow = function () {
+                    setTimeout(notification.close, 15000)
+                }
+            }
+        }
+
+    } else {
+        document.body.innerHTML("Failed to load the data");
+    }
+}
+setInterval(showError, 1 * 60 * 1000)
